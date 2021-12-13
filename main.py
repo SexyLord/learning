b555e470b36ca0b06e2d3bea770f3e35ed55fc48
SexyLord <tolendi,tastybay@gmail.com>
2021-12-14 01:56:53 +0600

    first blood

---
+++ main.py
@@ -0,0 +1,13 @@
+# HUiniya
+# # print("Hello muzerfucker")
+# word = "suckers"
+# print(word)
+# personName=	"Tolik" #string str
+# print('my name is ' +  personName)
+personage= "23" #integer int 
+print ('i am ' + str(personage))
+# # massa = 2.23 #float
+# # print(type(massa)) #определение тайп оф variables
+# # IsEmpty = False #boolean bool
+# # print (IsEmpty)
+
